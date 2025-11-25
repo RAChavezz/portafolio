d3e6d949cefaee938f9f4bf242ccd709da6d9b87
Ricardo Chavez <ricardo_chavez@swifttrans.com>
2025-11-25 14:43:05 -0600

    whitespace-nowrap

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -19,7 +19,7 @@ export default function Hero() {
             </a>
           </div>
         </div>
-        <div className="lg:ml-auto flex items-start lg:justify-end gap-2">
+        <div className="lg:ml-auto flex items-start lg:justify-end gap-2 whitespace-nowrap">
           <a
             href={profile.links.find(l => l.label.toLowerCase().includes('linkedin'))?.href}
             target="_blank"
